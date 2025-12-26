d8aeee2bd17701ac26f84883ac95af1ace34b261
praveentechdelite <org.vipstech@gmail.com>
2025-12-26 13:44:36 +0530

    login added

--- app.py
+++ app.py
@@ -1,12 +1,42 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, session
 
 app = Flask(__name__)
+app.secret_key = "super_secret_key"  # change in production
+
+USERNAME = "admin"
+PASSWORD = "admin123"
 
 STREAMS = [f"drone{i}" for i in range(1, 11)]
 
-@app.route("/")
+
+@app.route("/", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        if username == USERNAME and password == PASSWORD:
+            session["logged_in"] = True
+            return redirect(url_for("dashboard"))
+        else:
+            return render_template("login.html", error="Invalid Username or Password")
+
+    return render_template("login.html")
+
+
+@app.route("/dashboard")
 def dashboard():
+    if not session.get("logged_in"):
+        return redirect(url_for("login"))
+
     return render_template("dashboard.html", streams=STREAMS)
+
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect(url_for("login"))
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
